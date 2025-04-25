ff8cb79496efba0e27e7ab9847fedf515ffb2e69
Chris Swarbrick <chris.swarb@gmail.com>
2025-04-25 22:23:57 +0100

    Update localhost ref

--- scripts/seedDailyChallenges.js
+++ scripts/seedDailyChallenges.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const Daily = require('../models/Daily');
 
-mongoose.connect('mongodb://localhost:27017/watchle', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://0.0.0.0:27017/watchle', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const seedChallenges = async () => {
   const challenges = [
