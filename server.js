806953d38ffffe3cb49331105e8d2373fd1a0f3d
Chris Swarbrick <chris.swarb@gmail.com>
2025-04-26 13:41:17 +0100

    Drop console

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log('process.env.MONGO_URI: ', process.env.MONGO_URI)
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
