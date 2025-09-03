0bdddc91136c69de49dcde36bb7618319ddc9c09
Chris Swarbrick <chris.swarb@gmail.com>
2025-09-03 21:16:07 +0100

    Update images

--- scripts/seedDailyChallenges.js
+++ scripts/seedDailyChallenges.js
@@ -1,4 +1,3 @@
-// filepath: /Users/chrisswarbrick/git/watchle_full_project/backend/scripts/seedDailyChallenges.js
 const mongoose = require('mongoose');
 const Daily = require('../models/Daily');
 
@@ -7,28 +6,76 @@ mongoose.connect('mongodb://127.0.0.1:27017/watchle', { useNewUrlParser: true, u
 const seedChallenges = async () => {
   const challenges = [
     {
-      date: '2025-04-24',
+      date: '2025-09-4',
       imageSet: [
-        'zoom0.jpg',
-        'zoom1.jpg',
-        'zoom2.jpg',
-        'zoom3.jpg',
-        'zoom4.jpg'
+        '1a.jpg',
+        '2a.jpg',
+        '3a.jpg',
+        '4a.jpg',
+        '5a.jpg'
       ],
-      watchMake: 'Rolex',
-      watchModel: 'Submariner'
+      watchMake: 'Audemars Piguet',
+      watchModel: 'Grand Complication'
     },
     {
-      date: '2025-04-25',
+      date: '2025-09-5',
       imageSet: [
-        'zoom0.jpg',
-        'zoom1.jpg',
-        'zoom2.jpg',
-        'zoom3.jpg',
-        'zoom4.jpg'
+        '1b.jpg',
+        '2b.jpg',
+        '3b.jpg',
+        '4b.jpg',
+        '5b.jpg'
       ],
-      watchMake: 'Omega',
-      watchModel: 'Seamaster'
+      watchMake: 'Tudor',
+      watchModel: 'Black Bay Chrono'
+    },
+    {
+      date: '2025-09-6',
+      imageSet: [
+        '1c.jpg',
+        '2c.jpg',
+        '3c.jpg',
+        '4c.jpg',
+        '5c.jpg'
+      ],
+      watchMake: 'Christopher Ward',
+      watchModel: 'Sealander GMT'
+    },
+    {
+      date: '2025-09-7',
+      imageSet: [
+        '1d.jpg',
+        '2d.jpg',
+        '3d.jpg',
+        '4d.jpg',
+        '5d.jpg'
+      ],
+      watchMake: 'Tag Heuer',
+      watchModel: 'Aquaracer'
+    },
+    {
+      date: '2025-09-8',
+      imageSet: [
+        '1e.jpg',
+        '2e.jpg',
+        '3e.jpg',
+        '4e.jpg',
+        '5e.jpg'
+      ],
+      watchMake: 'Vacheron Constantin',
+      watchModel: 'Perpetual Calendar'
+    },
+    {
+      date: '2025-09-9',
+      imageSet: [
+        '1f.jpg',
+        '2f.jpg',
+        '3f.jpg',
+        '4f.jpg',
+        '5f.jpg'
+      ],
+      watchMake: 'Cartier',
+      watchModel: 'Crash'
     }
   ];
 
